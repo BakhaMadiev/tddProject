91f0dd52ff251f6b7b3896571ae73ff20d6d527b
Bakhauddin Ziyouddinov <bakhamadiev1@gmail.com>
2025-07-28 22:53:25 +0200

    Test cases are updated

--- inventory/src/main/java/com/example/inventory/view/swing/LibrarySwingView.java
+++ inventory/src/main/java/com/example/inventory/view/swing/LibrarySwingView.java
@@ -157,11 +157,10 @@ public class LibrarySwingView extends JFrame implements BookView, AuthorView{
 			@Override
 			public void keyReleased(KeyEvent e) {
 				addAuthorButton.setEnabled(
-						!authorNameTextBox.getText().trim().isEmpty() &&
-						!authorIdTextBox.getText().trim().isEmpty() &&
-						!authorSurnameTextBox.getText().trim().isEmpty()
-					);
-				}
+					!authorNameTextBox.getText().trim().isEmpty() && 
+					!authorSurnameTextBox.getText().trim().isEmpty() &&
+					!authorIdTextBox.getText().trim().isEmpty());
+			}
 		});
 		authorNameTextBox.setName("authorNameTextBox");
 		authorNameTextBox.setColumns(10);
