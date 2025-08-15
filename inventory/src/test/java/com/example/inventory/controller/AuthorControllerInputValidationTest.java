cbfe527aadf4ac416eca5dcf0b7d747ad1c14f76
Bakhauddin Ziyouddinov <bakhamadiev1@gmail.com>
2025-08-15 15:19:05 +0200

    Tests fix

--- inventory/src/test/java/com/example/inventory/controller/AuthorControllerInputValidationTest.java
+++ inventory/src/test/java/com/example/inventory/controller/AuthorControllerInputValidationTest.java
@@ -22,49 +22,49 @@ import com.example.inventory.view.AuthorView;
 @RunWith(Parameterized.class)
 public class AuthorControllerInputValidationTest {
 	@Mock
-    private AuthorRepository authorRepo;
-
-    @Mock
-    private AuthorView authorView;
-
-    @InjectMocks
-    private AuthorController authorController;
-
-    private AutoCloseable closeable;
-
-    private Author author;
-    private String expectedMessage;
-    
-    @Before
-    public void setup() {
-        closeable = MockitoAnnotations.openMocks(this);
-    }
-
-    @After
-    public void releaseMocks() throws Exception {
-        closeable.close();
-    }
-
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][] {
-            { new Author("1", null, "Surname"), "Please, set a correct name for author with ID: 1" },
-            { new Author("1", " ", "Surname"), "Please, set a correct name for author with ID: 1" },
-            { new Author("1", "Name", null), "Please, set a correct surname for author with ID: 1" },
-            { new Author("1", "Name", " "), "Please, set a correct surname for author with ID: 1" }
-        });
-    }
-
-    public AuthorControllerInputValidationTest(Author author, String expectedMessage) {
-        this.author = author;
-        this.expectedMessage = expectedMessage;
-    }
-
-    @Test
-    public void testInputValidation() {
-        authorController.newAuthor(author);
-        verify(authorView).showError(expectedMessage, author);
-        verifyNoMoreInteractions(authorRepo, authorView);
-    }
+	private AuthorRepository authorRepo;
+	
+	@Mock
+	private AuthorView authorView;
+	
+	@InjectMocks
+	private AuthorController authorController;
+	
+	private AutoCloseable closeable;
+	
+	private Author author;
+	private String expectedMessage;
+	
+	@Before
+	public void setup() {
+		closeable = MockitoAnnotations.openMocks(this);
+	}
+	
+	@After
+	public void releaseMocks() throws Exception {
+		closeable.close();	
+	}
+	
+	@Parameterized.Parameters
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] {
+			{ new Author("1", null, "Surname"), "Please, set a correct name for author with ID: 1" },
+			{ new Author("1", " ", "Surname"), "Please, set a correct name for author with ID: 1" },
+			{ new Author("1", "Name", null), "Please, set a correct surname for author with ID: 1" },
+			{ new Author("1", "Name", " "), "Please, set a correct surname for author with ID: 1" }
+		});
+	}
+	
+	public AuthorControllerInputValidationTest(Author author, String expectedMessage) {
+		this.author = author;
+		this.expectedMessage = expectedMessage;
+	}
+	
+	@Test
+	public void testInputValidation() {
+		authorController.newAuthor(author);
+		verify(authorView).showError(expectedMessage, author);
+		verifyNoMoreInteractions(authorRepo, authorView);
+	}
 
 }
